54af2df30c597af2e01b63294e2f04ea31748c9d
Alexander Pushkarev <alexander.pushkarev@codurance.com>
2021-05-21 18:15:37 +0100

    Improve test naming

--- java/maven-kickstart-project/src/test/java/com/codurance/katalyst/kickstart/CalculatorShould.java
+++ java/maven-kickstart-project/src/test/java/com/codurance/katalyst/kickstart/CalculatorShould.java
@@ -2,38 +2,21 @@ package com.codurance.katalyst.kickstart;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.junit.MockitoJUnitRunner;
-
-import java.util.Iterator;
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
-@RunWith(MockitoJUnitRunner.class)
 public class CalculatorShould {
 
-    Calculator summator;
+    Calculator calculator;
 
     @Before
     public void setUp() {
-        summator = new Calculator();
+        calculator = new Calculator();
     }
 
     @Test
     public void returnSumOfNumbers() {
-        assertEquals(6, summator.sum(4, 2));
-    }
-
-    @Test
-    public void pleaseIgnoreMeIAmJustTestingMockito() {
-        Iterator iterator = mock(Iterator.class);
-        when(iterator.next()).thenReturn("Hello").thenReturn("World");
-
-        String result =iterator.next() + " " +iterator.next();
-
-        assertEquals("Hello World", result);
+        assertEquals(6, calculator.sum(4, 2));
     }
 
 }
---
+++ java/maven-kickstart-project/src/test/java/com/codurance/katalyst/kickstart/MockitoShould.java
@@ -0,0 +1,31 @@
+package com.codurance.katalyst.kickstart;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class MockitoShould {
+
+    public class MockedClass {
+
+        public Boolean returnFalse() {
+            return null;
+        }
+    }
+
+    @Test
+    public void mockInstanceMethods() {
+
+        MockedClass mockedObject = mock(MockedClass.class);
+        when(mockedObject.returnFalse()).thenReturn(true);
+
+        assertFalse(mockedObject.returnFalse());
+    }
+}
