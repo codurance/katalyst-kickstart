24fad5e7fc36cc37e3dfe13a91f8b88821a888fd
Alexander Pushkarev <alexander.pushkarev@codurance.com>
2021-05-25 00:41:45 +0300

    Add parametrized test example

--- java/maven-kickstart-project/src/test/java/com/codurance/katalyst/kickstart/CalculatorShould.java
+++ java/maven-kickstart-project/src/test/java/com/codurance/katalyst/kickstart/CalculatorShould.java
@@ -2,12 +2,39 @@ package com.codurance.katalyst.kickstart;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
 
 import static org.junit.Assert.assertEquals;
 
+
+@RunWith(Parameterized.class)
 public class CalculatorShould {
 
-    Calculator calculator;
+    private Calculator calculator;
+
+    private int num1;
+    private int num2;
+    private int expectedResult;
+
+    @Parameterized.Parameters
+    public static Collection primeNumbers() {
+        return Arrays.asList(new Object[][] {
+                { 2, 3, 5 },
+                { 6, 0, 6 },
+                { 4, -2, 2 }
+        });
+    }
+
+    public CalculatorShould(int num1, int num2,
+                            int expectedResult){
+        this.num1 = num1;
+        this.num2 = num2;
+        this.expectedResult = expectedResult;
+    }
 
     @Before
     public void setUp() {
@@ -20,6 +47,14 @@ public class CalculatorShould {
     @Test
     public void returnSumOfNumbers() {
         assertEquals(6, calculator.sum(4, 2));
+    }
+
+    /**
+     * This method demonstrates a parametrized test case
+     */
+    @Test
+    public void returnSumOfNumbersParametrized() {
+        assertEquals(this.expectedResult, calculator.sum(this.num1, this.num2));
     }
 
 }
