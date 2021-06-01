41b3f6060832589cc760d498a65cd5482d0f7db5
Alexander Pushkarev <alexander.pushkarev@codurance.com>
2021-06-01 15:44:56 +0300

    Rename project folder to

--- java/maven-kickstart-project/src/main/java/com/codurance/katalyst/kickstart/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.codurance.katalyst.kickstart.repository;
-
-import com.codurance.katalyst.kickstart.model.User;
-
-public interface UserRepository {
-    User getUser(String userName);
-
-    int add(User newUser);
-}
--- java/maven-kickstart-project/src/main/java/com/codurance/katalyst/kickstart/service/DuplicateUserException.java
+++
@@ -1,6 +0,0 @@
-package com.codurance.katalyst.kickstart.service;
-
-public class DuplicateUserException extends Throwable {
-    public DuplicateUserException(String s) {
-    }
-}
