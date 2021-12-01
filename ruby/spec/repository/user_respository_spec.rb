27ad0a760e22774ca7cf88738b6cc1ccf8144c23
gregory <gregory.havenga@gmail.com>
2021-12-01 12:01:49 +0200

    Spec boilerplate for all classes to prevent future load issues creeping in

---
+++ ruby/spec/repository/user_respository_spec.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+require_relative '../../model/user'
+require_relative '../../repository/user_repository'
+
+RSpec.describe UserRepository do
+  describe ".get" do
+    context "when the user exists" do
+      xit "returns the user" do
+
+      end
+    end
+
+    context "when the user does not exist" do
+      xit "returns nil" do
+
+      end
+    end
+  end
+
+  describe ".add" do
+    context "when the user exists" do
+      xit "does not add the user" do
+
+      end
+    end
+
+    context "when the user does not exist" do
+      xit "add the user" do
+
+      end
+    end
+  end
+end
---
+++ ruby/spec/service/user_service_spec.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative '../../model/user'
+require_relative '../../repository/user_repository'
+require_relative '../../service/user_service'
+
+RSpec.describe UserService do
+end
